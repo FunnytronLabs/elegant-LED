61138f87b2b75b1acef9195600387119fcbdc9eb
Funnytron Labs <167612794+FunnytronLabs@users.noreply.github.com>
2024-06-28 15:52:08 +1000

    Update elegant_LED.cpp

--- src/elegant_LED.cpp
+++ src/elegant_LED.cpp
@@ -1,1 +1,97 @@
+//
 
+// === INCLUDES === //
+#include "elegant_LED.h"
+#include "Arduino.h"
+
+
+// === METHODS === //
+elegant_LED::elegant_LED(int channel, int bit_resolution, int frequency, int pin)
+{
+  this->channel        = channel;
+  this->bit_resolution = bit_resolution;
+  this->duty_max       = (1<<bit_resolution) - 1;
+  this->frequency      = frequency;
+  this->pin            = pin;
+  
+  ledcSetup(channel, frequency, bit_resolution);
+  ledcAttachPin(pin, channel);
+}
+
+void elegant_LED::on(void)
+{
+  ledcWrite(channel, duty_max);
+}
+
+void elegant_LED::off(void)
+{
+  ledcWrite(channel, 0);
+}
+
+void elegant_LED::rising_pulse(unsigned long period)
+{
+  static unsigned long time_start = millis();
+  
+  unsigned long time_now = millis();
+  unsigned long time_difference = time_now - time_start;
+  
+  if(time_difference > period){
+    time_start = millis(); //time_start = time_now - time_difference;
+    time_difference = 0; //time_difference -= period;
+  }
+
+  unsigned int duty_now = (time_difference * duty_max) / period; // = gradient * time_difference : gradient = duty_max/period
+  
+  ledcWrite(channel, duty_now);
+}
+
+void falling_pulse(unsigned long period) // !!! NEEDS CORRECTING !!!
+{
+  static unsigned long time_start = millis();
+  
+  unsigned long time_now = millis();
+  unsigned long time_difference = time_now - time_start;
+  
+  if(time_difference > period){
+    time_start = millis(); //time_start = time_now - time_difference;
+    time_difference = 0; //time_difference -= period;
+  }
+
+  unsigned int duty_now = (time_difference * duty_max) / period; // = gradient * time_difference : gradient = duty_max/period
+  
+  ledcWrite(channel, duty_now);
+}
+
+void rising_falling_pulse(unsigned long period) // !!! NEEDS CORRECTING !!!
+{
+  static unsigned long time_start = millis();
+  
+  unsigned long time_now = millis();
+  unsigned long time_difference = time_now - time_start;
+  
+  if(time_difference > period){
+    time_start = millis(); //time_start = time_now - time_difference;
+    time_difference = 0; //time_difference -= period;
+  }
+
+  unsigned int duty_now = (time_difference * duty_max) / period; // = gradient * time_difference : gradient = duty_max/period
+  
+  ledcWrite(channel, duty_now);
+}
+
+void sine_pulse(unsigned long period) // !!! NEEDS CORRECTING !!!
+{
+  static unsigned long time_start = millis();
+  
+  unsigned long time_now = millis();
+  unsigned long time_difference = time_now - time_start;
+  
+  if(time_difference > period){
+    time_start = millis(); //time_start = time_now - time_difference;
+    time_difference = 0; //time_difference -= period;
+  }
+
+  unsigned int duty_now = (time_difference * duty_max) / period; // = gradient * time_difference : gradient = duty_max/period
+  
+  ledcWrite(channel, duty_now);
+}
